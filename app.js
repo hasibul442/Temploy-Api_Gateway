bb1408c91cc08bcff5eac2bbc8e8b0270e69ed1c
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-10-22 23:30:21 +0600

    refactor: reorganize app.js for improved readability and structure

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-    // index.js
-    import express from 'express';
+// index.js
+import express from 'express';
+import dotenv from "dotenv";
+import connectDB from './src/utils/dbconnection.js';
+import routes from './src/routes/routes.js';
 
-    const app = express();
-    const port = 3000;
+dotenv.config();
+const app = express();
+const port = 3000;
 
-    app.get('/', (req, res) => {
-      res.send('Hello from ES6 Express!');
-    });
 
-    app.listen(port, () => {
-      console.log(`Server running on http://localhost:${port}`);
-    });
+// Middleware
+app.use(express.json());
+connectDB();
+
+// Routes
+app.use('/v1/api', routes);
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
