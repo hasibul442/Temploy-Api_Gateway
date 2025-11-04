8480bc004f41fc337ebdce868ad95a682b8223bf
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-04 19:07:28 +0600

    feat: refactor error handling middleware to export functions and improve structure

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,6 @@
-const errorHandler = (err, req, res, next) => {
+import { response } from "express";
+
+export function errorHandler(err, req, res, next) {
   console.error("🔥 Error:", err.message);
   res.status(500).json({
     success: false,
@@ -6,4 +8,13 @@ const errorHandler = (err, req, res, next) => {
   });
 };
 
-export default errorHandler;
+
+// Not found error response
+export function notFound(req, res, next) {
+  res.status(404).json({
+    status: false,
+    success: false,
+    message: `Route not found - ${req.originalUrl}`,
+    path: req.originalUrl,
+  });
+};
