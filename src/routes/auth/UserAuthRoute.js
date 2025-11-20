1f27fa8a1ea2650d4ec6d4523a1834ced2e4e99e
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-20 11:40:36 +0600

    Add UserAuthRoute for authentication endpoints

---
+++ src/routes/auth/UserAuthRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { login, profile, registration } from "../../controllers/auth/UserAuthController.js";
+
+
+const router = express.Router();
+
+router.post("/register", registration);
+router.post("/login", login);
+router.get("/profile", profile);
+
+export default router;
