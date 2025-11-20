193c950092e12d03260942fb6d510cfda256335a
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-20 11:39:08 +0600

    Implement user authentication controller methods

---
+++ src/controllers/auth/UserAuthController.js
@@ -0,0 +1,59 @@
+import { getUserProfile, userLogin, userRegister } from "../../services/auth/UserAuthServices.js";
+
+
+export async function registration(req, res, next) {
+  try {
+    const responseData = await userRegister(req);
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: "User registered successfully",
+      ...responseData,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      success: false,
+      auth_status: false,
+      message: error.message,
+    });
+  }
+}
+
+export async function login(req, res, next) {
+    try {
+        const responseData = await userLogin(req);
+        res.status(200).json({
+            status: 200,
+            success: true,
+            message: "User logged in successfully",
+            ...responseData
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 400,
+            success: false,
+            auth_status: false,
+            message: error.message
+        });
+    }
+}
+
+export async function profile(req, res, next) {
+    try {
+        const responseData = await getUserProfile(req);
+        res.status(200).json({
+            status: 200,
+            success: true,
+            message: "User profile fetched successfully",
+            ...responseData
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 400,
+            success: false,
+            auth_status: false,
+            message: error.message
+        });
+    }
+}
