e3272884e08704d22b44fe1be895145465368117
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-12-03 11:49:32 +0600

    Add OTP verification route to UserAuthRoute

--- src/routes/auth/UserAuthRoute.js
+++ src/routes/auth/UserAuthRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, profile, registration, userDelete } from "../../controllers/auth/UserAuthController.js";
+import { login, profile, registration, userDelete, otpValidation } from "../../controllers/auth/UserAuthController.js";
 
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/register", registration);
 router.post("/login", login);
 router.get("/profile", profile);
 router.delete("/delete/:id", userDelete);
+router.post("/otp/verification", otpValidation);
 
 export default router;
