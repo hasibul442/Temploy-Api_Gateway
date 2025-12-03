48ef5a32dddd835828fa3dea110f3aaba9191a1f
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-12-03 10:26:34 +0600

    Refactor user authentication methods and add deleteUser

--- src/controllers/auth/UserAuthController.js
+++ src/controllers/auth/UserAuthController.js
@@ -1,5 +1,9 @@
-import { getUserProfile, userLogin, userRegister } from "../../services/auth/UserAuthServices.js";
-
+import {
+  deleteUser,
+  getUserProfile,
+  userLogin,
+  userRegister,
+} from "../../services/auth/UserAuthServices.js";
 
 export async function registration(req, res, next) {
   try {
@@ -21,39 +25,56 @@ export async function registration(req, res, next) {
 }
 
 export async function login(req, res, next) {
-    try {
-        const responseData = await userLogin(req);
-        res.status(200).json({
-            status: 200,
-            success: true,
-            message: "User logged in successfully",
-            ...responseData
-        });
-    } catch (error) {
-        res.status(400).json({
-            status: 400,
-            success: false,
-            auth_status: false,
-            message: error.message
-        });
-    }
+  try {
+    const responseData = await userLogin(req);
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: "User logged in successfully",
+      ...responseData,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      success: false,
+      auth_status: false,
+      message: error.message,
+    });
+  }
 }
 
 export async function profile(req, res, next) {
-    try {
-        const responseData = await getUserProfile(req);
-        res.status(200).json({
-            status: 200,
-            success: true,
-            message: "User profile fetched successfully",
-            ...responseData
-        });
-    } catch (error) {
-        res.status(400).json({
-            status: 400,
-            success: false,
-            auth_status: false,
-            message: error.message
-        });
-    }
+  try {
+    const responseData = await getUserProfile(req);
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: "User profile fetched successfully",
+      ...responseData,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      success: false,
+      auth_status: false,
+      message: error.message,
+    });
+  }
+}
+
+export async function userDelete(req, res, next) {
+  try {
+    const responseData = await deleteUser(req.params.id);
+    res.status(200).json({
+      status: 200,
+      success: true,
+      ...responseData,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      success: false,
+      message: error.message,
+    });
+  }
 }
